7ef1f2a9879b479b88a85cdd4b1116d79c1ebb78
Andrei <andrei@palchys.com>
2021-11-29 21:58:58 +0200

    wip

--- server/src/migrations/index.ts
+++ server/src/migrations/index.ts
@@ -7,6 +7,9 @@ import { ResumeMigration1632333725126 } from './1632333725126-ResumeMigration';
 import { User1635365797478 } from './1635365797478-User';
 import { StageInterview1637591194886 } from './1637591194886-StageInterview';
 import { StudentFeedback1637686351309 } from './1637686351309-StudentFeedback';
+import { StudentFeedback1638214356373 } from './1638214356373-StudentFeedback';
+import { StudentFeedback1638215020747 } from './1638215020747-StudentFeedback';
+import { StudentFeedback1638215898561 } from './1638215898561-StudentFeedback';
 
 export const migrations = [
   UserMigration1630340371992,
@@ -18,4 +21,7 @@ export const migrations = [
   User1635365797478,
   StageInterview1637591194886,
   StudentFeedback1637686351309,
+  StudentFeedback1638214356373,
+  StudentFeedback1638215020747,
+  StudentFeedback1638215898561,
 ];
