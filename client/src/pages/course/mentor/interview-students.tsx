1cfc569c8588bc32889d6c7a1cdaba7790134f52
Andrei <andrei@palchys.com>
2021-12-04 12:51:36 +0200

    wip

--- client/src/pages/course/mentor/interview-students.tsx
+++ client/src/pages/course/mentor/interview-students.tsx
@@ -1,5 +1,7 @@
 import { Table } from 'antd';
-import { PageLayout, withSession, Rating } from 'components';
+import withSession from 'components/withSession';
+import { Rating } from 'components/Rating';
+import { PageLayout } from 'components/PageLayout';
 import {
   getColumnSearchProps,
   numberSorter,
